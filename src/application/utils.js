a0ac415096102627b3119d3fa682d188df3950a9
loocdoc <tokochagunava@yahoo.com>
2023-02-08 20:27:06 +0400

    refresh token

---
+++ src/application/utils.js
@@ -0,0 +1,7 @@
+import decode from 'jwt-decode'
+
+export const checkTokenValidity = (token) => {
+    const expirationDate = decode(token).exp;
+    const isExpired = expirationDate * 1000 < new Date().getTime();
+    return isExpired;
+};
