c7bd73a41a58280fb846d85dba109dbca6c5c37b
loocdoc <tokochagunava@yahoo.com>
2023-02-23 18:23:20 +0400

    side bar and search

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,8 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// import axios from "axios";
 import { axiosInstance } from "../../application";
 
-export const authenticateUser = createAsyncThunk(
+export const authentificateUser = createAsyncThunk(
     "user/authenticateUser",
     async (values, { rejectWithValue }) => {
         try {
@@ -12,42 +11,43 @@ export const authenticateUser = createAsyncThunk(
             localStorage.setItem("refresh_token", data.refreshToken);
             return data;
         } catch (error) {
-            return rejectWithValue("error during login")
-
+            return rejectWithValue("error during login");
         }
     }
 );
 
-const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        userInfo: null,
-        loading: false,
-        error: null,
-    },
-    reducers: {
-        logoutUser: (state) => {
-            state.userInfo = null;
-            localStorage.removeItem("token");
-            localStorage.removeItem("refresh_token");
+const userSlice = createSlice(
+    {
+        name: "user",
+        initialState: {
+            userInfo: null,
+            loading: false,
+            error: null,
         },
-    },
+        reducers: {
+            logoutUser: (state) => {
+                state.userInfo = null;
+                localStorage.removeItem("token");
+                localStorage.removeItem("refresh_token");
+            },
+        },
+        extraReducers: (builder) => {
+            builder.addCase(authentificateUser.pending, (state) => {
+                state.loading = true;
+            });
+            builder.addCase(authentificateUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.userInfo = action.payload.user;
+                state.error = null;
+            });
+            builder.addCase(authentificateUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload
+            })
+        },
 
-    extraReducers: (builder) => {
-        builder.addCase(authenticateUser.pending, (state) => {
-            state.loading = true;
-        });
-        builder.addCase(authenticateUser.fulfilled, (state, action) => {
-            state.loading = false;
-            state.userInfo = action.payload.user;
-            state.error = null;
-        });
-        builder.addCase(authenticateUser.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload
-        });
-    },
-});
+    }
+);
 
-export const userReducer = userSlice.reducer
+export const userReducer = userSlice.reducer;
 export const { logoutUser } = userSlice.actions;
